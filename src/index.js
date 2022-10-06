38978bbab14c517dc5915898f5c2f302a111e8df
ozgeosbourne <ozgenurayyasar@gmail.com>
2022-10-06 14:51:14 +0300

    Added new functions on JS

---
+++ src/index.js
@@ -0,0 +1,88 @@
+//1
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+
+let li = document.querySelector("li#current-date");
+li.innerHTML = `${day}, ${hour}:${minutes}`;
+//2
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = `${searchInput.value}`;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+//3
+function swap(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = `73°`;
+}
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", swap);
+
+function reverse(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = `23°`;
+}
+let celcius = document.querySelector("#celcius");
+celcius.addEventListener("click", reverse);
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `Humidity: ${Math.round(
+    response.data.main.humidity
+  )}%`;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+  document.querySelector("#forecast").innerHTML = response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let city = document.querySelector("#search-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocationButton = document.querySelector("#current-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
